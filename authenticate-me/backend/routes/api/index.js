969723a8caa74df491c629911674c177ebd01a35
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-09-27 11:03:59 -0700

    UPDATE: THIS COMMIT IS PHASE 3, LAST COMMIT IS 2

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,49 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
+
+// THE FOLLOWING 3 ROUTES ARE FOR TESTING OUR AUTH MIDDLEWARE FUNCTIONS; THEY HAVE BEEN COMMENTED OUT SINCE WE HAVE CONFIRMED THAT THEY DO IN FACT WORK.
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+
+// // GET /api/restore-user
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+// // GET /api/require-auth
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+
 
 module.exports = router;
