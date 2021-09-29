a2f4fbb3cd970b7d4766af216763b1ea015181ce
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-09-28 17:08:08 -0700

    FINISH USER LOGIN SETUP PHASE 3

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -6,6 +6,13 @@ const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
