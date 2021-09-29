08b82d7bd9e35d391b076f3258c2b8c62d64ee45
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-09-29 16:37:46 -0700

    ADD SESSION USER LOGIN ABILITY WITH REDUX STORE

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -8,6 +8,8 @@ import configureStore from './store';
 
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
+import * as sessionActions from './store/session';
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
@@ -15,11 +17,12 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-}
+// if (process.env.NODE_ENV !== "production") {
+//   window.store = store;
+// }
 
 function Root() {
   return (
