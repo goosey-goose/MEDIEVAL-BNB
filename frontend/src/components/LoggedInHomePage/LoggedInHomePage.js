8af303b26bb055979bd5a0dc4c9c24267da7849f
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-10-12 11:57:50 -0700

    COMMENTED A LINE OUT

--- frontend/src/components/LoggedInHomePage/LoggedInHomePage.js
+++ frontend/src/components/LoggedInHomePage/LoggedInHomePage.js
@@ -1,4 +1,3 @@
-// frontend/src/components/LoginFormPage/index.js
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserBookings } from '../../store/booking';
