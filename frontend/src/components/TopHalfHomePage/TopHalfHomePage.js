96a641cc88cf618802be094c6bd462011f278d8f
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-12-01 21:15:46 -0800

    FIRST COMMIT WITH HAMBURGER MENU

--- frontend/src/components/TopHalfHomePage/TopHalfHomePage.js
+++ frontend/src/components/TopHalfHomePage/TopHalfHomePage.js
@@ -1,9 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TopHalfHomePage.css';
 
 function TopHalfHomePage() {
+  let menu;
+  let closeIcon;
+  let menuIcon;
+
+  function toggleMenu() {
+    console.log("hamburger menu clicked");
+    if (menu.classList.contains("showMenu")) {
+      console.log("A");
+      menu.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      console.log("B");
+      menu.classList.add("showMenu");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+    }
+  }
+
+  useEffect(() => {
+    menu = document.querySelector(".menu");
+    const menuItems = document.querySelectorAll(".menuItem");
+    const hamburger= document.querySelector(".hamburger");
+    closeIcon= document.querySelector(".closeIcon");
+    menuIcon = document.querySelector(".menuIcon");
+
+    hamburger.addEventListener("click", toggleMenu);
+
+    menuItems.forEach(
+      function(menuItem) {
+        menuItem.addEventListener("click", toggleMenu);
+      }
+    )
+  }, [])
+
   return (
-    <div id="homepage-top-grid"></div>
+    <div id="homepage-top-grid">
+      <header>
+        Medieval BNB
+        <nav>
+          <ul className="menu">
+            <li><a className="menuItem" href="#">Home</a></li>
+            <li><a className="menuItem" href="#">Food</a></li>
+            <li><a className="menuItem" href="#">Places</a></li>
+            <li><a className="menuItem" href="#">Things</a></li>
+          </ul>
+          <button className="hamburger">
+            <i className="menuIcon">menu</i>
+            <i className="closeIcon">close</i>
+          </button>
+        </nav>
+      </header>
+    </div>
   );
 }
 
