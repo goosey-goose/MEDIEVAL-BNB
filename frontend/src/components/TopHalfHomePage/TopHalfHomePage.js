9049a10851d3b054e910eb9828dc96f1c400cc53
goosey-goose <g.ebenezer.miranda@gmail.com>
2021-12-02 18:15:10 -0800

    MODIFY TOGGLE MENU FUNCTION

--- frontend/src/components/TopHalfHomePage/TopHalfHomePage.js
+++ frontend/src/components/TopHalfHomePage/TopHalfHomePage.js
@@ -2,33 +2,35 @@ import React, { useState, useEffect } from 'react';
 import './TopHalfHomePage.css';
 
 function TopHalfHomePage() {
-  // let menu;
-  // let closeIcon;
-  // let menuIcon;
 
-  function toggleMenu() {
+  const toggleMenu = (event) => {
+    event.stopPropagation();
     const menu = document.querySelector(".menu");
     const closeIcon= document.querySelector(".closeIcon");
     const menuIcon = document.querySelector(".menuIcon");
-    if (menu.classList.contains("showMenu")) {
-      console.log("A");
-      menu.classList.remove("showMenu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
-    } else {
-      console.log("B");
-      menu.classList.add("showMenu");
-      closeIcon.style.display = "block";
-      menuIcon.style.display = "none";
+    if (event.target.className === "menuIcon" || event.target.className === "closeIcon") {
+      if (menu.classList.contains("showMenu")) {
+        // console.log("A");
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+      } else {
+        // console.log("B");
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+      }
+    } else if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
     }
   }
 
   useEffect(() => {
-    // menu = document.querySelector(".menu");
     const menuItems = document.querySelectorAll(".menuItem");
     const hamburger= document.querySelector(".hamburger");
-    // closeIcon= document.querySelector(".closeIcon");
-    // menuIcon = document.querySelector(".menuIcon");
+    const bodyElement = document.getElementsByTagName("body");
 
     hamburger.addEventListener("click", toggleMenu);
 
@@ -37,6 +39,8 @@ function TopHalfHomePage() {
         menuItem.addEventListener("click", toggleMenu);
       }
     )
+
+    bodyElement[0].addEventListener('click', toggleMenu);
   }, [])
 
   return (
