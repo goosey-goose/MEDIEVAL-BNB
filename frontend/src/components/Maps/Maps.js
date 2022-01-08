a68fb191b97da50937a4de8ce7aedb68d3ddfa2f
goosey-goose <g.ebenezer.miranda@gmail.com>
2022-01-08 14:27:18 -0800

    RESIZE MAP AREA DIMENSIONS; CHANGE MAP CENTER

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px',
+  // width: '700px',
+  // height: '700px',
+  width: '100%',
+  height: '82rem',
 };
 
 const center = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 53.58007771089779,
+  lng: -1.9821929670470553,
 };
 
 const Maps = ({ apiKey }) => {
@@ -24,7 +26,12 @@ const Maps = ({ apiKey }) => {
           mapContainerStyle={containerStyle}
           center={center}
           zoom={8}
-        />
+        >
+          <Marker
+            position={{lat: 53.58007771089779, lng: -1.9821929670470553}}>
+
+          </Marker>
+        </GoogleMap>
       )}
     </>
   );
