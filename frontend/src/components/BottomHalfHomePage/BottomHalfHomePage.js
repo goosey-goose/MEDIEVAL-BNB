454fdb7042b5c5b25cb139d520d1837783f6b72d
goosey-goose <g.ebenezer.miranda@gmail.com>
2022-01-07 19:01:42 -0800

    ADD FUNCTIONALITY TO SWITCH BETWEEN TILES AND MAP TABS

--- frontend/src/components/BottomHalfHomePage/BottomHalfHomePage.js
+++ frontend/src/components/BottomHalfHomePage/BottomHalfHomePage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllSpots } from '../../store/spot';
 // import Maps from '../Maps/Maps';
-// import MapContainer from '../Maps/index.js';
+import MapContainer from '../Maps/index.js';
 import './BottomHalfHomePage.css';
 
 function BottomHalfHomePage() {
@@ -16,7 +16,20 @@ function BottomHalfHomePage() {
       if (event.currentTarget === tab) {
         tab.classList.add("selected-tab");
       } else tab.classList.remove("selected-tab");
-    })
+    });
+    if (event.currentTarget.innerText === "Map") {
+      let outerMostMapContainerDiv = document.getElementById("outermost-map-container-div");
+      outerMostMapContainerDiv.classList.remove("hide");
+      let homepageBottomGrid = document.getElementById("homepage-bottom-grid");
+      homepageBottomGrid.classList.remove("show-tiles");
+      homepageBottomGrid.classList.add("hide-tiles");
+    } else if (event.currentTarget.innerText === "Tiles") {
+      let outerMostMapContainerDiv = document.getElementById("outermost-map-container-div");
+      outerMostMapContainerDiv.classList.add("hide");
+      let homepageBottomGrid = document.getElementById("homepage-bottom-grid");
+      homepageBottomGrid.classList.remove("hide-tiles");
+      homepageBottomGrid.classList.add("show-tiles");
+    }
   }
 
   let tabs;
@@ -41,12 +54,15 @@ function BottomHalfHomePage() {
         <div className="default-tab-style">Map</div>
         <div className="empty-tab-2"></div>
       </div>
-      <div id="homepage-bottom-grid">
+      <div id="homepage-bottom-grid" className="show-tiles">
         {allSpots && allSpots.map((spot, index) => {
           return (<div className="test-div" key={index}>
             <img alt="" src={spot.imageUrl}></img>
           </div>)
         })}
+      </div>
+      <div id="outermost-map-container-div" className="hide">
+        <MapContainer></MapContainer>
       </div>
     </>
   );
