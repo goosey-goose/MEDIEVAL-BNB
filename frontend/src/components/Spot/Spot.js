25c8ac891f97d9e950bd364c5a41ef45b443285f
goosey-goose <g.ebenezer.miranda@gmail.com>
2022-01-14 20:17:52 -0800

    ADJUST SPOT GRID TO DISPLAY SPOT IMAGE IN ITS ORIGINAL SCALE; WHEN SPOT COMPONENT RENDERS, IT CHANGES THE MIN-WIDTH OF  ROOT TO ADJUST TO WIDTH OF SPOT GRID

--- frontend/src/components/Spot/Spot.js
+++ frontend/src/components/Spot/Spot.js
@@ -3,15 +3,38 @@ import Header from '../Header/Header.js';
 import './Spot.css';
 
 function Spot() {
+
+    useEffect(() => {
+        let root = document.getElementById("root");
+        root.style.minWidth = "56.25rem";
+
+        return () => {
+            root.style.minWidth = "23rem";
+        }
+    });
+
+
     return (
         <div id="spot-outer-container-div">
             <div id="spot-outer-grid">
-                <div style={{backgroundColor: "red"}}></div>
-                <div style={{backgroundColor: "yellow"}}></div>
-                <div style={{backgroundColor: "green"}}></div>
-                <div id="spot-outer-grid_child_4">
-                    <div id="child_4-a"></div>
-                    <div id="child_4-b"></div>
+                <div id="spot-info-1">
+                    <div>
+                        Spot Title
+                    </div>
+                    <div>
+                        Spot Address
+                    </div>
+                </div>
+                <div id="spot-info-2">
+                    <div>
+                        {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Eilean_Donan_Castle%2C_Scotland_-_Jan_2011.jpg/383px-Eilean_Donan_Castle%2C_Scotland_-_Jan_2011.jpg" /> */}
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Rochester_Castle_Keep_and_Bailey_0038stcp.JPG/600px-Rochester_Castle_Keep_and_Bailey_0038stcp.JPG" />
+                        {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Eilean_Donan_Castle%2C_Scotland_-_Jan_2011.jpg/383px-Eilean_Donan_Castle%2C_Scotland_-_Jan_2011.jpg" /> */}
+                    </div>
+                </div>
+                <div id="spot-info-3">
+                    <div id="spot-info-3_child_1">1</div>
+                    <div id="spot-info-3_child_2">2</div>
                 </div>
             </div>
         </div>
